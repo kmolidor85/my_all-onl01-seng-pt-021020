4db25eacadd18c4c844a47a052b7b2e9b0705cf4
kyle molidor <k_molidor@yahoo.com>
2020-02-22 22:25:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,13 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i + 1
+  end
+  
+  if block_return_values.include?(false)
+    false
+  else
+    TRUE
   end
 end
